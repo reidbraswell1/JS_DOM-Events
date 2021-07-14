d6c7fab25a38885ac5b561750b86ad5f979944ff
Reid Braswell <reidbras@gmail.com>
2021-07-14 18:05:46 -0500

    Finalizing changes and comments for exercise 4 and 5

--- main.js
+++ main.js
@@ -3,12 +3,14 @@
 // Select Node #1 and change the text to: "I used the getElementById("node1") method to access this"
 document.getElementById("node1").innerText =
   'I used the getElementById("node1") method to access this';
+
 // Select Node #2 and change the text to: "I used the getElementByClassName("node2") method to access this" */
 let node2Class = document.getElementsByClassName("node2");
 for (let i = 0; i < node2Class.length; i++) {
   node2Class[i].innerText =
     'I used the getElementByClassName("node2") method to access this';
 }
+
 // Select ALL the h3 tags and change the text to: "I used the getElementByTagName("h3") method to access all of these" */
 let h3Elements = document.getElementsByTagName("h3");
 for (let i = 0; i < h3Elements.length; i++) {
@@ -49,6 +51,7 @@ let newTextNode = document.createTextNode("New Child Node");
 newParagraph.appendChild(newTextNode);
 parent2.replaceChild(newParagraph, n1);
 
+// Remove the new child node
 let n2 = document.getElementById("N2");
 while (n2.hasChildNodes()) {
   n2.removeChild(n2.firstChild);
@@ -99,14 +102,19 @@ function move() {
     // We are moving left to right accross the bottom of the container and
     // have reached the container width - box width.
     // Now we want to move from bottom to top. Set the box position
-    // to the container width - box width and increment from the top 
+    // to the container height - box height and increment from the top 
     // in a negative direction
   } else if (leftRightMovement && boxPosition >= container.offsetWidth - box.offsetWidth) {
-    boxPosition = container.offsetWidth - box.offsetWidth;
+    boxPosition = container.offsetHeight - box.offsetHeight;
     leftRightMovement = false;
     rightLeftMovement = false;
     topBottomMovement = false;
     bottomTopMovement = true;
+    // We are moving bottom to top on the right side of the
+    // container and have reached the top. We now need to 
+    // set the box position to the container width minus the box width
+    // and move from right to left again across the top of the 
+    // container.
   } else if (bottomTopMovement && boxPosition <= 0) {
     boxPosition = container.offsetWidth - box.offsetWidth;
     rightLeftMovement = true;
@@ -135,24 +143,27 @@ function move() {
 // This div should be a 'modal' that covers the main content on the screen
 // BONUS: The modal popup should be able to be closed. Refactor for this functionality
 function show() {
+  // Define a div for the outer container
   let modalContainer = document.createElement("div");
   modalContainer.id="modal-container";
 
+  // Define span element for the close element
   let modalClose = document.createElement("span");
   modalClose.innerHTML="&times;"
-  //modalClose.append(modalCloseText);
-  modalClose.className="modal-close";
+  modalClose.id="modal-close";
   modalClose.onclick=function() { modalContainer.style.display="none"; }
   
+  // Define an element to hold the text content
   let modalContent = document.createElement("h3");
   modalContent.id="modal-content";
-  let modalTextNode = document.createTextNode("Clicking the button triggers the onclick event, which calls the JS function show()... which alerts the user");
-  modalContent.appendChild(modalTextNode);
+  modalContent.innerHTML="Clicking the button triggers the onclick event, <br> which calls the JS function show()... which alerts the user <br> To close this modal click the &times; in the upper right hand corner.";
   modalContent.appendChild(modalClose);
 
+  // Add the close button and the content to the container
   modalContainer.appendChild(modalClose);
   modalContainer.appendChild(modalContent);
 
+  // Get the div object for exercise5 and append the modal container to the exercise5 div
   let exercise5Container = document.getElementsByClassName("exercise5");
   exercise5Container[0].appendChild(modalContainer);
 }
